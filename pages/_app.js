d0ad0561fa9874dc0828283610b46fd35dada08a
tprole <91798428+tprole@users.noreply.github.com>
2023-06-24 09:24:35 -0400

    moving the card

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import NextApp from 'next/app'
-import { Box, Card, Heading, Text } from 'theme-ui' // Import the components needed for the card
 
 
 import '@hackclub/theme/fonts/reg-bold.css'
@@ -14,14 +13,6 @@ export default class App extends NextApp {
     return (
       <ThemeProvider theme={theme}>
         <ColorSwitcher />
-
-        {/* Add the Card component with the desired content */}
-        <Card>
-          <Box p={3}>
-            <Heading as="h2">Card Title</Heading>
-            <Text>This is the content of the card.</Text>
-          </Box>
-        </Card>
 
         <Component {...pageProps} />
       </ThemeProvider>
